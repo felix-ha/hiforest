c88dcb77ea15aa722ee21c62b0532098228051ff
hauerf98 <hauerf98@gmail.com>
2025-11-12 20:57:46 +0100

    Calculate number of features

--- main.hs
+++ main.hs
@@ -8,21 +8,29 @@ type Dataset = [[Double]]
 x :: Dataset
 x = [[1.2, 1.1], [1.9, 0.2], [2.2, 0.1], [1.9, 0.2]]
 
+numberOfFeatures :: Dataset -> Int
+numberOfFeatures x | length distinct_values == 1 = distinct_values !! 0
+                   | otherwise = -1
+   where distinct_values = distinctValues (map length x) []
+
 distinctValues :: Eq a => [a] -> [a] -> [a]
 distinctValues [] ds = ds
 distinctValues (x:xs) ds | x `elem` ds = distinctValues xs ds 
                          | otherwise = distinctValues xs (x:ds) 
 
--- TODO: Return value if distinctValues equals 1
-numberOfFeatures :: Dataset -> Int
-numberOfFeatures x = length (distinctValues (map length x) [])
-
+isValidDataset :: Dataset -> Bool
+isValidDataset ds = length distinct_values == 1
+   where distinct_values = distinctValues (map length x) []
 
 testFeatures :: Test
 testFeatures = TestCase (assertEqual "Test numberOfFeatures for x" 2 (numberOfFeatures x))
 
+testisValidDataset:: Test
+testisValidDataset = TestCase (assertEqual "Test isValidDataset for x" True (isValidDataset x))
+
 tests :: Test
-tests = TestList [TestLabel "testFeatures" testFeatures]
+tests = TestList [TestLabel "testFeatures" testFeatures, 
+                  TestLabel "testIsValid" testisValidDataset]
 
 main :: IO ()
 main = do
